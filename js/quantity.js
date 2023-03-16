8a2498614ef908d34776f2f93720ab2aeae408be
Percy <percival.w.bradley@gmail.com>
2023-03-15 22:05:27 -0700

    JS Checkout Authenticated

--- js/quantity.js
+++ js/quantity.js
@@ -1,18 +1,33 @@
 'use strict';
 
 
-function increaseValue() {
+function increaseValue1() {
   var value = parseInt(document.getElementById('number').value, 10);
   value = isNaN(value) ? 0 : value;
   value++;
-  document.getElementById('number').value = value;
+  document.getElementById('number1').value = value;
 }
 
-function decreaseValue() {
+function decreaseValue1() {
   var value = parseInt(document.getElementById('number').value, 10);
   value = isNaN(value) ? 0 : value;
   value < 1 ? value = 1 : '';
   value--;
-  document.getElementById('number').value = value;
+  document.getElementById('number1').value = value;
+}
+
+function increaseValue2() {
+  var value = parseInt(document.getElementById('number').value, 10);
+  value = isNaN(value) ? 0 : value;
+  value++;
+  document.getElementById('number2').value = value;
+}
+
+function decreaseValue2() {
+  var value = parseInt(document.getElementById('number').value, 10);
+  value = isNaN(value) ? 0 : value;
+  value < 1 ? value = 1 : '';
+  value--;
+  document.getElementById('number2').value = value;
 }
 
