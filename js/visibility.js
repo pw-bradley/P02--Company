a89d88e7ca25dbe22a846cb8cab5b75e82ed951f
Percy <percival.w.bradley@gmail.com>
2023-03-15 20:56:45 -0700

    Menu Complete

---
+++ js/visibility.js
@@ -0,0 +1,25 @@
+/* Based on code provided here: https://www.w3schools.com/cssref/pr_class_visibility.php*/
+
+function hideTea() {
+    document.getElementById("tea").style.visibility = "hidden";
+}
+
+function displayTea() {
+    document.getElementById("tea").style.visibility = "visible";
+}
+
+function hideConCookie() {
+    document.getElementById("conCookie").style.visibility = "hidden";
+}
+
+function displayConCookie() {
+    document.getElementById("conCookie").style.visibility = "visible";
+}
+
+function hideGalCookie() {
+    document.getElementById("galCookie").style.visibility = "hidden";
+}
+
+function displayGalCookie() {
+    document.getElementById("galCookie").style.visibility = "visible";
+}
